49c672a082d774e1cc1e475412fcbe1daadf2f44
Albert <acastemoreno@gmail.com>
2020-04-13 12:07:45 -0500

    add instance creattion and usage

--- week-1/day-1/calculator.rb
+++ week-1/day-1/calculator.rb
@@ -23,3 +23,12 @@ class Calculator
     a % b
   end
 end
+
+# Usage, copy and paste this to make sure your program works correctly.
+calculator = Calculator.new
+puts "Sum result is #{calculator.add(5, 4)}" # Should output "Sum result is 9" 
+puts "Subtraction result is #{calculator.subtract(5, 4)}" # Should output "Subtraction result is 1" 
+puts "Division result is #{calculator.divide(12, 4)}" # Should output "Division result is 3"
+puts "Multiplication result is #{calculator.multiply(5, 4)}" # Should output "Multiplication result is 20" 
+puts "Exponentiation result is #{calculator.exponentiate(2, 3)}" # Should output "Exponentiation result is 8" 
+puts "Modulo result is #{calculator.modulo(5, 2)}" # Should output "Modulo result is 1" 
